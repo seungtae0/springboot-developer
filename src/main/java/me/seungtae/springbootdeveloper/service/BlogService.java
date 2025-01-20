bd591a6b7b2e218d243468af3b443cdc330c983c
seungtae_lg <eumc95@naver.com>
2025-01-20 22:25:21 +0900

    글 목록 조회 기능 추가

--- src/main/java/me/seungtae/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/seungtae/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,8 @@ import me.seungtae.springbootdeveloper.dto.AddArticleRequest;
 import me.seungtae.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
@@ -14,5 +16,9 @@ public class BlogService {
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
